2de1ec4fcac3eb63c1df480ee598ccf859dfce98
Ra Phearom <phearom123@gmail.com>
2026-01-04 22:31:34 +0700

    Initial stock prediction web app

--- app.py
+++ app.py
@@ -124,4 +124,6 @@ def download_file(filename):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Port set by cloud platform
+    app.run(debug=False, host="0.0.0.0", port=port)
